7f8c9a2720019d1c01a93116f59978a77af7c00c
Nicolas FARES <145838261+nicofares@users.noreply.github.com>
2024-10-04 08:44:33 +0200

    Create bertin22.py

---
+++ utilities/bertin22.py
@@ -0,0 +1,8 @@
+# Functions to compute forces 
+# EHD forces of a sphere moving in a viscous liquid near an elastic wall
+# Ref: Bertin et al. JFM 2022
+
+import numpy as np
+
+def test(x)
+  return x
